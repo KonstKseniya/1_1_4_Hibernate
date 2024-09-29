913b279e3eecefc04df5ed8741d065c759c327be
KonstKseniya <kolody.xenia@yandex.ru>
2024-09-29 07:58:56 +0300

    Add files via upload

---
+++ PP_1_1_3-4_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,29 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 25);
+        userService.saveUser("Петр", "Петров", (byte) 26);
+        userService.saveUser("Константин", "Константинов", (byte) 27);
+        userService.saveUser("Степан", "Степанов", (byte) 28);
+
+        List<User> users = userService.getAllUsers();
+        System.out.println(users);
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+
+        Util.closeSessionFactory();
+    }
+}
